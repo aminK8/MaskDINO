2270b40c560352cf899adbd69ef5633a7a6478db
Amin <karimimonsefi.1@osu.edu>
2024-05-31 19:36:50 -0400

    fix bug

--- maskdino/data/higharc_categories.py
+++ maskdino/data/higharc_categories.py
@@ -1,7 +1,7 @@
 
 
 HIGHARC_CATEGORIES = [
-    {'color': [250, 141, 255], 'isthing': 1, 'id': 0, 'name': 'architectural-plans-kBh5'},
+    {'color': [250, 141, 255], 'isthing': 0, 'id': 0, 'name': 'architectural-plans-kBh5'},
     {'color': [220, 20, 60], 'isthing': 1, 'id': 1, 'name': 'bath'},
     {'color': [119, 11, 32], 'isthing': 1, 'id': 2, 'name': 'bed_closet'}, 
     {'color': [0, 0, 142], 'isthing': 1, 'id': 3, 'name': 'bed_pass'},
@@ -31,14 +31,14 @@ HIGHARC_CATEGORIES = [
     {'color': [0, 125, 92], 'isthing': 1, 'id': 27, 'name': 'pantry'}, 
     {'color': [209, 0, 151], 'isthing': 1, 'id': 28, 'name': 'patio'},
     {'color': [188, 208, 182], 'isthing': 1, 'id': 29, 'name': 'portico'},
-    {'color': [0, 220, 176], 'isthing': 0, 'id': 30, 'name': 'powder'},
-    {'color': [255, 99, 164], 'isthing': 0, 'id': 31, 'name': 'reach_closet'},
-    {'color': [92, 0, 73], 'isthing': 0, 'id': 32, 'name': 'reading_nook'},
-    {'color': [133, 129, 255], 'isthing': 0, 'id': 33, 'name': 'rear_porch'}, 
-    {'color': [78, 180, 255], 'isthing': 0, 'id': 34, 'name': 'solarium'}, 
-    {'color': [0, 228, 0], 'isthing': 0, 'id': 35, 'name': 'stairs_editor'}, 
-    {'color': [174, 255, 243], 'isthing': 0, 'id': 36, 'name': 'util_hall'},
-    {'color': [45, 89, 255], 'isthing': 0, 'id': 37, 'name': 'walk'},
-    {'color': [134, 134, 103], 'isthing': 0, 'id': 38, 'name': 'water_closet'},
-    {'color': [145, 148, 174], 'isthing': 0, 'id': 39, 'name': 'workshop'},
+    {'color': [0, 220, 176], 'isthing': 1, 'id': 30, 'name': 'powder'},
+    {'color': [255, 99, 164], 'isthing': 1, 'id': 31, 'name': 'reach_closet'},
+    {'color': [92, 0, 73], 'isthing': 1, 'id': 32, 'name': 'reading_nook'},
+    {'color': [133, 129, 255], 'isthing': 1, 'id': 33, 'name': 'rear_porch'}, 
+    {'color': [78, 180, 255], 'isthing': 1, 'id': 34, 'name': 'solarium'}, 
+    {'color': [0, 228, 0], 'isthing': 1, 'id': 35, 'name': 'stairs_editor'}, 
+    {'color': [174, 255, 243], 'isthing': 1, 'id': 36, 'name': 'util_hall'},
+    {'color': [45, 89, 255], 'isthing': 1, 'id': 37, 'name': 'walk'},
+    {'color': [134, 134, 103], 'isthing': 1, 'id': 38, 'name': 'water_closet'},
+    {'color': [145, 148, 174], 'isthing': 1, 'id': 39, 'name': 'workshop'},
 ]
