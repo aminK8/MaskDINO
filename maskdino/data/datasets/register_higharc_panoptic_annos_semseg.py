f9acb890034dc20ee9ab321d7e529d7123be30f7
Amin <karimimonsefi.1@osu.edu>
2024-05-31 01:01:10 -0400

    fix dataset registeration

--- maskdino/data/datasets/register_higharc_panoptic_annos_semseg.py
+++ maskdino/data/datasets/register_higharc_panoptic_annos_semseg.py
@@ -24,12 +24,6 @@ _PREDEFINED_SPLITS_COCO_PANOPTIC = {
 
 def get_metadata():
     meta = {}
-    # The following metadata maps contiguous id from [0, #thing categories +
-    # #stuff categories) to their names and colors. We have to replica of the
-    # same name and color under "thing_*" and "stuff_*" because the current
-    # visualization function in D2 handles thing and class classes differently
-    # due to some heuristic used in Panoptic FPN. We keep the same naming to
-    # enable reusing existing visualization functions.
     thing_classes = [k["name"] for k in HIGHARC_CATEGORIES if k["isthing"] == 1]
     thing_colors = [k["color"] for k in HIGHARC_CATEGORIES if k["isthing"] == 1]
     stuff_classes = [k["name"] for k in HIGHARC_CATEGORIES]
@@ -40,24 +34,13 @@ def get_metadata():
     meta["stuff_classes"] = stuff_classes
     meta["stuff_colors"] = stuff_colors
 
-    # Convert category id for training:
-    #   category id: like semantic segmentation, it is the class id for each
-    #   pixel. Since there are some classes not used in evaluation, the category
-    #   id is not always contiguous and thus we have two set of category ids:
-    #       - original category id: category id in the original dataset, mainly
-    #           used for evaluation.
-    #       - contiguous category id: [0, #classes), in order to train the linear
-    #           softmax classifier.
+
     thing_dataset_id_to_contiguous_id = {}
     stuff_dataset_id_to_contiguous_id = {}
 
     for i, cat in enumerate(HIGHARC_CATEGORIES):
         if cat["isthing"]:
             thing_dataset_id_to_contiguous_id[cat["id"]] = i
-        # else:
-        #     stuff_dataset_id_to_contiguous_id[cat["id"]] = i
-
-        # in order to use sem_seg evaluator
         stuff_dataset_id_to_contiguous_id[cat["id"]] = i
 
     meta["thing_dataset_id_to_contiguous_id"] = thing_dataset_id_to_contiguous_id
@@ -66,17 +49,7 @@ def get_metadata():
     return meta
 
 
-def load_coco_panoptic_json(json_file, image_dir, gt_dir, semseg_dir, meta):
-    """
-    Args:
-        image_dir (str): path to the raw dataset. e.g., "~/coco/train2017".
-        gt_dir (str): path to the raw annotations. e.g., "~/coco/panoptic_train2017".
-        json_file (str): path to the json file. e.g., "~/coco/annotations/panoptic_train2017.json".
-    Returns:
-        list[dict]: a list of dicts in Detectron2 standard format. (See
-        `Using Custom Datasets </tutorials/datasets.html>`_ )
-    """
-
+def load_coco_panoptic_json(json_file, image_dir, panoptic_root, semseg_dir, meta):
     def _convert_category_id(segment_info, meta):
         if segment_info["category_id"] in meta["thing_dataset_id_to_contiguous_id"]:
             segment_info["category_id"] = meta["thing_dataset_id_to_contiguous_id"][
@@ -96,12 +69,8 @@ def load_coco_panoptic_json(json_file, image_dir, gt_dir, semseg_dir, meta):
     ret = []
     for ann in json_info["annotations"]:
         image_id = int(ann["image_id"])
-        # TODO: currently we assume image and label has the same filename but
-        # different extension, and images have extension ".jpg" for COCO. Need
-        # to make image extension a user-provided argument if we extend this
-        # function to support other COCO-like datasets.
         image_file = os.path.join(image_dir, os.path.splitext(ann["file_name"])[0] + ".jpg")
-        label_file = os.path.join(gt_dir, ann["file_name"])
+        label_file = os.path.join(panoptic_root, ann["file_name"])
         sem_label_file = os.path.join(semseg_dir, ann["file_name"])
         segments_info = [_convert_category_id(x, meta) for x in ann["segments_info"]]
         ret.append(
@@ -121,18 +90,17 @@ def load_coco_panoptic_json(json_file, image_dir, gt_dir, semseg_dir, meta):
 
 
 def register_coco_panoptic_annos_sem_seg(
-    name, metadata, image_root, panoptic_root, panoptic_json, sem_seg_root, instances_json
+    name, metadata, image_root, panoptic_root, panoptic_json, sem_seg_root, instances_json, meta
 ):
     panoptic_name = name
-    # delattr(MetadataCatalog.get(panoptic_name), "thing_classes")
-    # delattr(MetadataCatalog.get(panoptic_name), "thing_colors")
+    DatasetCatalog.register(
+        panoptic_name,
+        lambda: load_coco_panoptic_json(panoptic_json, image_root, panoptic_root, sem_seg_root, metadata),
+    )
     MetadataCatalog.get(panoptic_name).set(
-        thing_classes=metadata["thing_classes"],
-        thing_colors=metadata["thing_colors"],
-        # thing_dataset_id_to_contiguous_id=metadata["thing_dataset_id_to_contiguous_id"],
+       meta
     )
 
-    # the name is "coco_2017_train_panoptic_with_sem_seg" and "coco_2017_val_panoptic_with_sem_seg"
     semantic_name = name + "_with_sem_seg"
     DatasetCatalog.register(
         semantic_name,
@@ -142,7 +110,6 @@ def register_coco_panoptic_annos_sem_seg(
         sem_seg_root=sem_seg_root,
         panoptic_root=panoptic_root,
         image_root=image_root,
-        panoptic_json=panoptic_json,
         json_file=instances_json,
         evaluator_type="coco_panoptic_seg",
         ignore_label=255,
@@ -150,31 +117,19 @@ def register_coco_panoptic_annos_sem_seg(
         **metadata,
     )
     
-def higharch_dataset_function():
-    pass
 
 def register_all_coco_panoptic_annos_sem_seg(root):
     for (
         prefix,
-        (panoptic_root, panoptic_json, semantic_root),
+        (image_root, panoptic_json, panoptic_root),
     ) in _PREDEFINED_SPLITS_COCO_PANOPTIC.items():
-        prefix_instances = prefix[: -len("_panoptic")]
-        print(f"prefix: {prefix}")
-        print(f"prefix_instances: {prefix_instances}")
-        print(f"panoptic_root: {panoptic_root}")
-        print(f"panoptic_json: {panoptic_json}")
-        print(f"semantic_root: {semantic_root}")
-        
-        # instances_meta = MetadataCatalog.get(prefix_instances)
-        # image_root, instances_json = instances_meta.image_root, instances_meta.json_file
-
         register_coco_panoptic_annos_sem_seg(
             prefix,
             get_metadata(),
-            root,
+            os.path.join(root, image_root),
             os.path.join(root, panoptic_root),
             os.path.join(root, panoptic_json),
-            os.path.join(root, semantic_root),
+            os.path.join(root, panoptic_root),
             panoptic_json,
         )
 
