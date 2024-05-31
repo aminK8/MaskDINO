37b62eaf23d57cf617a9a4352840a54f45630349
Amin <karimimonsefi.1@osu.edu>
2024-05-31 02:59:26 -0400

    add new files

---
+++ tt2.py
@@ -0,0 +1,24 @@
+import json
+import os
+
+def add_isthing_to_categories(json_file):
+    with open(json_file, 'r') as f:
+        data = json.load(f)
+        
+    if 'categories' in data:
+        for category in data['categories']:
+            category['isthing'] = 1
+        
+        with open(json_file, 'w') as f:
+            json.dump(data, f, indent=4)
+
+def process_json_files_in_directory(json_file):
+    add_isthing_to_categories(json_file)
+    print(f"Added 'isthing' to categories in {json_file}")
+
+# Change 'directory_path' to the directory containing your JSON files
+for key_path in ["valid", "test", "train"]:
+    base_url = "../../dataset/seg_object_detection/auto_translate_v4-3"
+    # base_url = "/Users/amin/Desktop/higharc/Datasets/Laleled-2024-05-29/auto_translate_v4.v3i.coco-segmentation"
+    # Directory containing images
+    process_json_files_in_directory(os.path.join(base_url, key_path, '_annotations.coco.json'))
