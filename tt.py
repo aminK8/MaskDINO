d8165a59b57addd3bc09cdb25a47e222e7c3d266
Amin <karimimonsefi.1@osu.edu>
2024-05-31 01:41:03 -0400

    fix

--- tt.py
+++ tt.py
@@ -78,8 +78,8 @@ def save_panoptic_segmentation(segmentation_masks, output_path):
     cv2.imwrite(output_path, segmentation_masks)
 
 for key_path in ["valid", "test", "train"]:
-    # base_url = "../../dataset/seg_object_detection/auto_translate_v4-3"
-    base_url = "/Users/amin/Desktop/higharc/Datasets/Laleled-2024-05-29/auto_translate_v4.v3i.coco-segmentation"
+    base_url = "../../dataset/seg_object_detection/auto_translate_v4-3"
+    # base_url = "/Users/amin/Desktop/higharc/Datasets/Laleled-2024-05-29/auto_translate_v4.v3i.coco-segmentation"
     # Directory containing images
     image_dir = os.path.join(base_url, '{}'.format(key_path))
 
