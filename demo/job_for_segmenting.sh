e8a371f2bab606dd2ef16ce02883c1632dc40cdc
Amin <karimimonsefi.1@osu.edu>
2024-06-18 14:53:40 -0700

    fix

--- demo/job_for_segmenting.sh
+++ demo/job_for_segmenting.sh
@@ -4,9 +4,9 @@
 #SBATCH --time=0-1:00:00
 
 
-#SBATCH --nodes=1 --ntasks-per-node=2
-#SBATCH --ntasks=2
-#SBATCH --gres=gpu:2
+#SBATCH --nodes=1 --ntasks-per-node=1
+#SBATCH --ntasks=1
+#SBATCH --gres=gpu:1
 
 
 #SBATCH --mail-type=BEGIN,END,FAIL
@@ -15,7 +15,7 @@
 ### change 5-digit MASTER_PORT as you wish, slurm will raise Error if duplicated with others
 ### change WORLD_SIZE as gpus/node * num_nodes
 export MASTER_PORT=12340
-export WORLD_SIZE=2
+export WORLD_SIZE=1
 
 ### get the first node name as master address - customized for vgg slurm
 ### e.g. master(gnodee[2-5],gnoded1) == gnodee2
@@ -30,5 +30,5 @@ module load miniconda3
 source activate maskdino
 
 
- srun python demo_2.py --config-file ./configs/coco/panoptic-segmentation/maskdino_higharc_R50_bs16_50ep_3s_dowsample1_2048.yaml \
+ python demo_2.py --config-file ./configs/coco/panoptic-segmentation/maskdino_higharc_R50_bs16_50ep_3s_dowsample1_2048.yaml \
   --input /fs/scratch/PAS0536/amin/MultiGen-20M/images/aesthetics_6_25_plus_3m --opts MODEL.WEIGHTS ../maskdino_r50_50ep_300q_hid2048_3sd1_panoptic_pq53.0.pth
